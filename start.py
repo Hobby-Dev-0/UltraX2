f0e71371a5f3d6e8cdb33bfd44560240785b1f10
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-09-08 09:26:24 +0530

    k

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja yaar
-system ("git clone -b new https://github.com/ULTRA-OP/ULTRA-X.git && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone -b main https://github.com/InternetAmethyst/mongoult.git && cd mongoult && python3 -m ULTRA")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
